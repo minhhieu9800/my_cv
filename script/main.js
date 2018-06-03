3d23c7da03cafc0e29f83b5a1f0b95f094078a25
hieuvt98 <tungdanhchotuan@gmail.com>
2018-06-03 23:38:29 +0700

    hieu
    edit name

---
+++ script/main.js
@@ -0,0 +1,22 @@
+window.onscroll = function () {
+    showMenu()
+};
+
+
+//function showMenu() {
+//    //    let elm = document.getElementsByTagName("h1")[0];
+//    let position1 = document.documentElement.scrollTop;
+//    let position2 = document.body.scrollTop;
+//    if (position1 > 100 || position2 > 100) {
+//        document.getElementById("menu1").style.opacity = "0";
+//        document.getElementById("menu2").style.opacity = "1";
+//    } else {
+//        document.getElementById("menu1").style.opacity = "1";
+//        document.getElementById("menu2").style.opacity = "0";
+//    }
+//}
+
+//function changeMenu() {
+//    const elm = document.getElementById("menu");
+//    elm.style.
+//}
